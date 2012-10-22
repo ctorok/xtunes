13727c399853d04deb58fa6873b1214fd71feb82
Catharina Torok <ctorok@Cathas-MBP.local>
2012-10-22 15:24:04 -0400

    added add songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,57 +1,34 @@
 class SongsController < ApplicationController
-  # GET /songs
-  # GET /songs.json
-  def index
-    @songs = Song.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @songs }
+  def create
+  @song = Song.new(params[:song])
+    if @song.save
+      redirect_to songs_path, notice: 'song was successfully created.'
+    else
+       render action: "new"
     end
   end
 
-  # GET /songs/1
-  # GET /songs/1.json
-  def show
-    @song = Song.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @song }
-    end
-  end
-
-  # GET /songs/new
-  # GET /songs/new.json
-  def new
-    @song = Song.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @song }
-    end
-  end
-
-  # GET /songs/1/edit
   def edit
     @song = Song.find(params[:id])
   end
 
-  # song /songs
-  # song /songs.json
-  def create
-    @song = Song.new(params[:song])
-
-    respond_to do |format|
-      if @song.save
-        format.html { redirect_to @song, notice: 'song was successfully created.' }
-        format.json { render json: @song, status: :created, location: @song }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @song.errors, status: :unprocessable_entity }
-      end
-    end
+  def index
+    @songs = Song.all
   end
+
+  def new
+    @song = Song.new
+  end
+  
+  def show
+    @song = Song.find(params[:id])
+  end
+
+
+  
+
+
+  
 
   # PUT /songs/1
   # PUT /songs/1.json

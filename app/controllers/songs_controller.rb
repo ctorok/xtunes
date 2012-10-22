62d2ab9e495d26e1d1cb358a05af910efbf9e461
Catharina Torok <ctorok@Cathas-MBP.local>
2012-10-22 15:32:53 -0400

    finished controller methods in songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+  
   def create
   @song = Song.new(params[:song])
     if @song.save
@@ -19,42 +20,24 @@ class SongsController < ApplicationController
   def new
     @song = Song.new
   end
-  
+
   def show
     @song = Song.find(params[:id])
   end
 
-
-  
-
-
-  
-
-  # PUT /songs/1
-  # PUT /songs/1.json
   def update
     @song = Song.find(params[:id])
-
-    respond_to do |format|
       if @song.update_attributes(params[:song])
-        format.html { redirect_to @song, notice: 'song was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to @song, notice: 'song was successfully updated.' 
       else
-        format.html { render action: "edit" }
-        format.json { render json: @song.errors, status: :unprocessable_entity }
+        render action: "edit" 
       end
-    end
   end
 
-  # DELETE /songs/1
-  # DELETE /songs/1.json
+
   def destroy
     @song = Song.find(params[:id])
     @song.destroy
-
-    respond_to do |format|
-      format.html { redirect_to songs_url }
-      format.json { head :no_content }
-    end
+      redirect_to songs_url 
   end
 end

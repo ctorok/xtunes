b3b68067edcefe4b3fa84790d4d29e20f2b0021b
Catharina Torok <ctorok@Cathas-MBP.local>
2012-10-22 10:18:26 -0400

    Initial renerated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xtunes::Application.config.secret_token = '69640ca1cd30f99b105a95debd7f24a22148498089492307acf2317ba5cd9a61b059c198a094d354ce2cdb81fa3604a88dc17a9c70eec8ca99c0f3e6f420ac39'

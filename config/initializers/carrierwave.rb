0bbbc26500abc280aab982230773a1086ca7548e
Catharina Torok <ctorok@gmail.com>
2012-10-23 16:01:51 -0400

    Carrierwave fix

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',       # required
+    :aws_access_key_id      => ENV['aws_access_key'],       # required
+    :aws_secret_access_key  => ENV['aws_secret_key']      # required
+   # :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = 'xTunes'                     # required
+ # config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
+  config.fog_public     = false                                   # optional, defaults to true
+ # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
+

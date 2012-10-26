24c736d6b1dea0ee06e133b5e8277341aac86a14
Catharina Torok <ctorok@gmail.com>
2012-10-26 09:12:57 -0400

    Added many:many join table between albums and genres

---
+++ db/migrate/20121026131102_album_genre_table.rb
@@ -0,0 +1,8 @@
+class AlbumGenreTable < ActiveRecord::Migration
+  def  change
+    create_table :albums_genres, :id => false do |t|
+      t.integer :album_id
+      t.integer :genre_id
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121026130735) do
+ActiveRecord::Schema.define(:version => 20121026131102) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(:version => 20121026130735) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "year"
+  end
+
+  create_table "albums_genres", :id => false, :force => true do |t|
+    t.integer "album_id"
+    t.integer "genre_id"
   end
 
   create_table "albums_songs", :id => false, :force => true do |t|
